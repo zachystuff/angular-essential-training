62f2ad34ff03bbe1243b553b211794c4f7bfcc6c
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-07-28 10:12:45 -0500

    template or templateURL? That is the question!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
+/* 
+In component, you can use template or template URL metadata to load markup. When using templateURL meta, be sure to use a relative path to a file containing the html to be loaded. Angular will load that file.
+
+*/
+
 @Component({
   selector: 'mw-app',
-  template: '<h1>My App</h1>'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {}
