fe5eb56cb2ef6089b881301419f8363298162e55
zachystuff <34614449+zachystuff@users.noreply.github.com>
2020-08-13 10:10:06 -0500

    Error handling and something-- else, yeah POST and alladat!

--- src/app/media-item.service.ts
+++ src/app/media-item.service.ts
@@ -1,57 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaItemService {
   constructor(private http: HttpClient) {}
-
-  mediaItems = [
-    {
-      id: 1,
-      name: 'Firebug',
-      medium: 'Series',
-      category: 'Science Fiction',
-      year: 2010,
-      watchedOn: 1294166565384,
-      isFavorite: false
-    },
-    {
-      id: 2,
-      name: 'The Small Tall',
-      medium: 'Movies',
-      category: 'Comedy',
-      year: 2015,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 3,
-      name: 'The Redemption',
-      medium: 'Movies',
-      category: 'Action',
-      year: 2016,
-      watchedOn: null,
-      isFavorite: false
-    }, {
-      id: 4,
-      name: 'Hoopers',
-      medium: 'Series',
-      category: 'Drama',
-      year: null,
-      watchedOn: null,
-      isFavorite: true
-    }, {
-      id: 5,
-      name: 'Happy Joe: Cheery Road',
-      medium: 'Movies',
-      category: 'Action',
-      year: 2015,
-      watchedOn: 1457166565384,
-      isFavorite: false
-    }
-  ];
 
   get(medium) {
     const getOptions = {
@@ -61,19 +17,24 @@ export class MediaItemService {
       .pipe(
         map((response: MediaItemsResponse) => {
           return response.mediaItems;
-        })
+        }),
+        catchError(this.handleError)
       );
   }
 
   add(mediaItem) {
-    this.mediaItems.push(mediaItem);
+    return this.http.post('mediaitems', mediaItem)
+      .pipe(catchError(this.handleError));
   }
 
   delete(mediaItem) {
-    const index = this.mediaItems.indexOf(mediaItem);
-    if (index >= 0) {
-      this.mediaItems.splice(index, 1);
-    }
+    return this.http.delete(`mediaitems/${mediaItem.id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error.message);
+    return throwError('A data error occured, please try again')
   }
 }
 
